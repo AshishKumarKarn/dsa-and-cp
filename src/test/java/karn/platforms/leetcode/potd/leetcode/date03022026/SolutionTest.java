1dfae2152b4ec0701f05ff2242b45b8c5d29bf8f
Ashish Karn <ashishkrkarn@gmail.com>
2026-02-03 10:13:48 +0530

    POTD Leetcode 3637 03/02/2026

---
+++ src/test/java/karn/platforms/leetcode/potd/leetcode/date03022026/SolutionTest.java
@@ -0,0 +1,16 @@
+package karn.platforms.leetcode.potd.leetcode.date03022026;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void isTrionic() {
+        Solution solution = new Solution();
+        assertTrue(solution.isTrionic(new int[]{1, 3, 5, 4, 2, 6}));
+        assertFalse(solution.isTrionic(new int[]{2, 1, 3}));
+        assertFalse(solution.isTrionic(new int[]{3, 3, 8, 1, 4, 6}));
+    }
+}

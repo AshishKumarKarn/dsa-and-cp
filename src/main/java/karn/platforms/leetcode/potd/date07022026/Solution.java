9181e2b990330f90791a394a449067d4db2c4660
Ashish Karn <ashishkrkarn@gmail.com>
2026-02-07 20:13:53 +0530

    POTD Leetcode 1653 07/02/2026

--- src/main/java/karn/platforms/leetcode/potd/date07022026/Solution.java
+++ src/main/java/karn/platforms/leetcode/potd/date07022026/Solution.java
@@ -4,10 +4,10 @@ public class Solution {
     public int minimumDeletions(String s) {
         int aCount = 0;
         int bCount = 0;
-        int[][] preAPostBCounts = new int[s.length()][2];
+        int[][] preBPostACounts = new int[s.length()][2];
         for (int i = 0, j = s.length() - 1; i < s.length(); i++) {
-            preAPostBCounts[i][0] = bCount;
-            preAPostBCounts[j][1] = aCount;
+            preBPostACounts[i][0] = bCount;
+            preBPostACounts[j][1] = aCount;
             if (s.charAt(i) == 'b') {
                 bCount++;
             }
@@ -19,7 +19,7 @@ public class Solution {
 
         int result = (int) 1e9;
         for (int i = 0; i < s.length(); i++) {
-            result = Math.min(result, preAPostBCounts[i][0] + preAPostBCounts[i][1]);
+            result = Math.min(result, preBPostACounts[i][0] + preBPostACounts[i][1]);
         }
         return result;
 

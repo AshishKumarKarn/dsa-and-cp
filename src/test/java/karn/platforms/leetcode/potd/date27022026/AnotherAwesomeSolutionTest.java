dd6bcb93c1fc166ea206b4a02f5618f48677e5ea
Ashish Karn <ashishkrkarn@gmail.com>
2026-03-01 00:05:46 +0530

    POTD Leetcode 1680 28/02/2026

--- src/test/java/karn/platforms/leetcode/potd/date27022026/AnotherAwesomeSolutionTest.java
+++ src/test/java/karn/platforms/leetcode/potd/date27022026/AnotherAwesomeSolutionTest.java
@@ -2,7 +2,8 @@ package karn.platforms.leetcode.potd.date27022026;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class AnotherAwesomeSolutionTest {
 

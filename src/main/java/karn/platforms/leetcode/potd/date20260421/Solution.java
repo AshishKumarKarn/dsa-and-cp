0785bc237e5628a9c2a706c6525bf58493656490
Ashish Karn <ashishkrkarn@gmail.com>
2026-04-21 11:30:28 +0530

    Leetcode 1722

---
+++ src/main/java/karn/platforms/leetcode/potd/date20260421/Solution.java
@@ -0,0 +1,75 @@
+package karn.platforms.leetcode.potd.date20260421;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+//https://leetcode.com/problems/minimize-hamming-distance-after-swap-operations/?envType=daily-question&envId=2026-04-21
+//1722. Minimize Hamming Distance After Swap Operations
+public class Solution {
+    private int[] rank;
+
+    public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
+        int[] parents = new int[source.length];
+        rank = new int[parents.length];
+        Arrays.fill(rank, 1);
+        for (int i = 0; i < parents.length; i++) {
+            parents[i] = i;
+        }
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        for (int i = 0; i < allowedSwaps.length; i++) {
+            int x = allowedSwaps[i][0];
+            int y = allowedSwaps[i][1];
+            union(x, y, parents);
+        }
+        for (int i = 0; i < source.length; i++) {
+            int p = find(i, parents);
+            Map<Integer, Integer> freq = map.getOrDefault(p, new HashMap<>());
+            freq.put(source[i], freq.getOrDefault(source[i], 0) + 1);
+            map.put(p, freq);
+        }
+        // System.out.println(map);
+        // System.out.println(Arrays.toString(parents));
+        int count = 0;
+        for (int i = 0; i < target.length; i++) {
+            int p = find(i, parents);
+            Map<Integer, Integer> freq = map.get(p);
+            if (freq.containsKey(target[i])) {
+                int cnt = freq.get(target[i]);
+                if (cnt == 1) {
+                    freq.remove(target[i]);
+                } else {
+                    freq.put(target[i], cnt - 1);
+                }
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void union(int i, int j, int[] parent) {
+        int rootI = find(i, parent);
+        int rootJ = find(j, parent);
+        if (rootI != rootJ) {
+            if (rank[rootI] < rank[rootJ]) {
+                parent[rootI] = rootJ;
+            } else if (rank[rootI] > rank[rootJ]) {
+                parent[rootJ] = rootI;
+            } else {
+                parent[rootI] = rootJ;
+                rank[rootJ]++;
+            }
+        }
+    }
+
+    private int find(int x, int[] parent) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x], parent);
+    }
+
+    public boolean connected(int i, int j, int[] parents) {
+        return find(i, parents) == find(j, parents);
+    }
+}
---
+++ src/test/java/karn/platforms/leetcode/potd/date20260421/SolutionTest.java
@@ -0,0 +1,20 @@
+package karn.platforms.leetcode.potd.date20260421;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTest {
+
+    @Test
+    void minimumHammingDistance() {
+        Solution solution = new Solution();
+        assertEquals(14, solution.minimumHammingDistance(
+                new int[]{50, 46, 54, 35, 18, 42, 26, 72, 75, 47, 50, 4, 54, 21, 18, 18, 61, 64, 100, 14},
+                new int[]{83, 34, 43, 73, 61, 94, 10, 68, 74, 31, 54, 46, 28, 60, 18, 18, 4, 44, 79, 92},
+                new int[][]{
+                        {1, 8}, {14, 17}, {3, 1}, {17, 10}, {18, 2}, {7, 12}, {11, 3}, {1, 15}, {13, 17}, {18, 19}, {0, 10}, {15, 19}, {0, 15}, {6, 7}, {7, 15}, {19, 4}, {7, 16}, {14, 18}, {8, 10}, {17, 0}, {2, 13}, {14, 10}, {12, 17}, {2, 9}, {6, 15}, {16, 18}, {2, 16}, {2, 6}, {4, 5}, {17, 5}, {10, 13}, {7, 2}, {9, 16}, {15, 5}, {0, 5}, {8, 0}, {11, 12}, {9, 7}, {1, 0}, {11, 17}, {4, 6}, {5, 7}, {19, 12}, {3, 18}, {19, 1}, {13, 18}, {19, 6}, {13, 6}, {6, 1}, {4, 2}
+                }
+        ));
+    }
+}
